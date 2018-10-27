ad1c919613f3ecaf1ce7a137f839bab9ecc85c05
Kawin <winzazx@gmail.com>
2018-10-28 04:09:21 +0700

    new

--- src/Regular.java
+++ src/Regular.java
@@ -1,21 +1,18 @@
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Regular {
+public class Regular   {
 	public static void main(String[]args) {
 		
-
-		
-	      String everything = getText();
+	      String everything = getText("C:\\Users\\Winza\\eclipse-workspace\\Regular-Expression\\assets\\test_emails.txt");
 	      String[] contents = everything.split("From r\\s");
-	      String temp="";
-	      
-	      
+
 	      System.out.println("Total Email Found : "+(contents.length-1));
 
 	      for(int i =1;i<contents.length;i++)
@@ -23,7 +20,7 @@ public class Regular {
 	      ////////////////////////Email-Sender/////////////////////////
 	      String patternfrom = "From:.*";
 	      
-	      String from = rex(patternfrom,contents[i]);
+	      String from = rex(patternfrom,contents[i],false);
 	      
 	      String pattern_sender = "\\w\\S+@.*\\w";	     
 	      String email =rex(pattern_sender,from,"Email-Sender");
@@ -35,7 +32,7 @@ public class Regular {
 	      
 	      //////////////////////// Recipient //////////////////////////
 	      String Pattern_to  = "To:.*";
-	      String to = rex(Pattern_to,contents[i]);
+	      String to = rex(Pattern_to,contents[i],false);
 	      
 	      String pattern_recipient  = "\\w\\S*@.*\\w";	     
 	      rex(pattern_recipient ,to,"Recipient-Email");
@@ -46,7 +43,7 @@ public class Regular {
 	      
 	      //////////////////////////Date///////////////////////////////
 	      String patterndate = "Date:.*";
-	      String date = rex(patterndate,contents[i]);
+	      String date = rex(patterndate,contents[i],false);
 	      
 	      String pattern_date_field = "\\d+\\s\\w+\\s\\d+";	     
 	      rex(pattern_date_field,date,"Date-Send");
@@ -54,16 +51,18 @@ public class Regular {
 	      
 	      //////////////////////Subject & Body ////////////////////////
 	      String patternsubject = "Subject: .*";
-	      String subject = rex(patternsubject,contents[i]);
+	      String subject = rex(patternsubject,contents[i],false);
 	      
 	      String pattern_subject = "\\s.*";	     
 	      rex(pattern_subject,subject,"Subject");
 	      
 	      String pattern_body = "Status:.+";
-	      String body = rexbody(pattern_body,contents[i]);
+	      String body = rex(pattern_body,contents[i],true);
 	      
 	      String [] bodysplit = body.split("Status:");
 	      bodysplit[1] = bodysplit[1].substring(3, bodysplit[1].length());
+	      
+	      count(pattern_body,bodysplit[1],true);
  
 	      System.out.println("Body : "+ bodysplit[1]);
 	      /////////////////////////////////////////////////////////////
@@ -88,28 +87,68 @@ public class Regular {
 	     return m.group(0);
 		
 	}
-	public static String rex(String pattern,String content) {
-		Pattern r = Pattern.compile(pattern);
+	public static String rex(String pattern,String content,Boolean all) {
+		Pattern r;
+		if (all) {
+		r = Pattern.compile(pattern,Pattern.DOTALL);
+		}
+		else {
+		r = Pattern.compile(pattern);
+		}
+		
 	     Matcher m = r.matcher(content);
-	     m.find();
-	         
-	     return m.group(0);
+	     if (m.find()) {
+	    	 return m.group();
+	     }else {
+	    	 return "None";
+	     }
 		
 	}
-	public static String rexbody(String pattern,String content) {
-		Pattern r = Pattern.compile(pattern,Pattern.DOTALL);
+	public static void count(String pattern,String content,Boolean all) {
+		Pattern r;
+		if (all) {
+		r = Pattern.compile("\\S*[a-zA-Z]\\s*",Pattern.DOTALL);
+		}
+		else {
+		r = Pattern.compile(pattern);
+		}
+		ArrayList<String> temp = new ArrayList<String>();
 	     Matcher m = r.matcher(content);
-	     m.find();
-	         
-	     return m.group(0);
+	     while(m.find())
+	     { temp.add(m.group());
+	    	 //System.out.println(m.group());
+	     }
+	  
+	        for(String d:temp) {
+	        	int count =0;
+	            for (String f:temp) {
+	            	if(d.equals(f)) {
+	            		count++;
+	            		
+	            	}
+	            }
+	            System.out.println(d+count);
+	            
+	        }
+	        /*
+	        String country = getText("C:\\Users\\Winza\\eclipse-workspace\\Regular-Expression\\assets\\countries-list.txt");
+	        String[] contents = country.split("\\r?\\n");
+	        for (int i=0;i<content.length();i++) {
+	        	String patterns = contents[i];
+	        	rex(patterns,content,contents[i]);
+	        }
+	        */
+	       
+	       
 		
 	}
 	
+
 	
-	public static String getText () {
+	public static String getText (String path) {
 		String line="";
-		//String path = "/Users/winzazx/Desktop/Regular-Expression/assets/test_emails.txt";
-		String path = "C:\\Users\\win-z\\Desktop\\Regular-Expression\\assets\\test_emails.txt";
+		
+		
 		File file = new File(path);
 		StringBuilder sb = new StringBuilder();
 		
